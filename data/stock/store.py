30c21f94114ff409216eb9f2c80df66222c8ea57
Anlin Winarto <anlinwinarto@berkeley.edu>
2014-04-11 12:35:19 -0700

    data/stock/store.py

--- data/stock/store.py
+++ data/stock/store.py
@@ -5,11 +5,14 @@ import csv
 allfiles = os.listdir('done')
 
 indexData = {}
+years = range(1995, 2012)
 for filename in allfiles:
     filepath = 'done/' + filename
     csvfile = open(filepath, 'rU')
     filereader = csv.reader(csvfile)
     yearlyData = {}
+    for year in years:
+    	yearlyData[year] = None
     for row in filereader:
         yearlyData[row[0]] = row[1]
     indexData[filename[0:3]] = yearlyData
