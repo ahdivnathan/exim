f10acd8fcc10087055933fa734993328b719552f
Oski <oski@ubuntu.(none)>
2014-04-11 11:44:21 -0700

    data/stock/store.py

--- data/stock/store.py
+++ data/stock/store.py
@@ -1,9 +1,17 @@
 import pandas
 import os
+import csv
 
 allfiles = os.listdir('done')
 
 indexData = {}
 for filename in allfiles:
     filepath = 'done/' + filename
-    indexData[filename[0:3]] = pandas.read_csv(filepath, header = None)
+    csvfile = open(filepath, 'rU')
+    filereader = csv.reader(csvfile)
+    yearlyData = {}
+    for row in filereader:
+        yearlyData[row[0]] = row[1]
+    indexData[filename[0:3]] = yearlyData
+
+indexDataFrame = pandas.DataFrame(indexData).transpose()
